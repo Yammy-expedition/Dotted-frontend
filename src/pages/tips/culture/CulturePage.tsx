886a447a11ec02aaee3481d2f0bf4bde175901e9
Kimbyul88 <byul88byul@gmail.com>
2025-03-01 10:18:24 +0900

    fix: fix

--- src/pages/tips/culture/CulturePage.tsx
+++ src/pages/tips/culture/CulturePage.tsx
@@ -70,58 +70,58 @@ const HeaderTitleBox = styled.div`
     }
   }
 `;
-const HeaderInputBox = styled.div`
-  width: 30vw;
-  height: 4rem;
-  display: flex;
-  background-color: ${({ theme }) => theme.colors.backgroundLayer1};
-  border-radius: 5px;
-  position: relative;
-  padding: 0.9rem 0;
-  @media (max-width: 700px) {
-    width: 50vw;
-  }
-  @media (max-width: 500px) {
-    width: 80vw;
-  }
-  select {
-    width: 7.5rem;
-    height: 100%;
-    padding: 0 1.2rem;
-    border: none;
-    border-right: 1px solid ${({ theme }) => theme.colors.gray600};
-    background-color: ${({ theme }) => theme.colors.backgroundLayer1};
-    color: ${({ theme }) => theme.colors.gray600};
-    font-size: 1.4rem;
-    font-weight: 400;
-    line-height: 4rem;
-    letter-spacing: -0.48px;
+// const HeaderInputBox = styled.div`
+//   width: 30vw;
+//   height: 4rem;
+//   display: flex;
+//   background-color: ${({ theme }) => theme.colors.backgroundLayer1};
+//   border-radius: 5px;
+//   position: relative;
+//   padding: 0.9rem 0;
+//   @media (max-width: 700px) {
+//     width: 50vw;
+//   }
+//   @media (max-width: 500px) {
+//     width: 80vw;
+//   }
+//   select {
+//     width: 7.5rem;
+//     height: 100%;
+//     padding: 0 1.2rem;
+//     border: none;
+//     border-right: 1px solid ${({ theme }) => theme.colors.gray600};
+//     background-color: ${({ theme }) => theme.colors.backgroundLayer1};
+//     color: ${({ theme }) => theme.colors.gray600};
+//     font-size: 1.4rem;
+//     font-weight: 400;
+//     line-height: 4rem;
+//     letter-spacing: -0.48px;
 
-    &:focus {
-      outline: none;
-    }
-  }
+//     &:focus {
+//       outline: none;
+//     }
+//   }
 
-  input {
-    width: 100%;
-    height: 100%;
-    padding: 0 1.2rem;
-    border: none;
-    background-color: ${({ theme }) => theme.colors.backgroundLayer1};
-    color: ${({ theme }) => theme.colors.gray700};
-    font-size: 1.5rem;
-    font-weight: 400;
-    line-height: 4rem;
+//   input {
+//     width: 100%;
+//     height: 100%;
+//     padding: 0 1.2rem;
+//     border: none;
+//     background-color: ${({ theme }) => theme.colors.backgroundLayer1};
+//     color: ${({ theme }) => theme.colors.gray700};
+//     font-size: 1.5rem;
+//     font-weight: 400;
+//     line-height: 4rem;
 
-    &:focus {
-      outline: none;
-    }
-  }
+//     &:focus {
+//       outline: none;
+//     }
+//   }
 
-  svg {
-    position: absolute;
-    right: 1.6rem;
-    top: 50%;
-    transform: translateY(-50%);
-  }
-`;
+//   svg {
+//     position: absolute;
+//     right: 1.6rem;
+//     top: 50%;
+//     transform: translateY(-50%);
+//   }
+// `;
